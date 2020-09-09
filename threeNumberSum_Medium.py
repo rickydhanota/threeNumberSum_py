9d6e1ce10715f10166b660a44c16f6eafba8bcf2
Ricky <rickdhanota@gmail.com>
2020-09-08 22:43:09 -0700

    finished the algo

--- threeNumberSum_Medium.py
+++ threeNumberSum_Medium.py
@@ -4,5 +4,22 @@
 # example array = [12,3,1,2,-6,5,-8,6], targetSum = 0
 # sample output should be [[-8,2,6], [-8,3,5], [-6,1,5]]
 
-def threeNumbersum(array, targetSum):
-    pass
+def threeNumberSum(array, targetSum):
+    array.sort() #sorts the array, built in python function
+    triplets = []
+    for i in range(len(array)-2):
+        left = i+1
+        right = len(array)-1
+        while left < right:
+            currentSum = array[i] + array[left] + array[right]
+            if currentSum == targetSum:
+                triplets.append([array[i], array[left], array[right]])
+                left += 1
+                right -= 1
+            elif currentSum < targetSum:
+                left += 1
+            elif currentSum > targetSum:
+                right -= 1
+    return triplets
+
+print(threeNumberSum([12,3,1,2,-6,5,-8,6], 0)) # sorted array would be [-8,-6, 1, 2, 3, 5, 6, 12]
