03137dbbbe1528927c85e83458af83065aead733
Ricky <rickdhanota@gmail.com>
2020-09-08 19:18:00 -0700

    added some more details to the functionality

--- threeNumberSum_Medium.py
+++ threeNumberSum_Medium.py
@@ -1,3 +1,8 @@
 # Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. The function should find all the triplets in the array that sum up to the target sum and return a 2 dimensional array of all these triplets. The numbers in each triplet should be ordered in ascending order, and the triplet themselves should be ordered in acsending order with respect to the numbers they hold.
 
 # If no threee numbers sum up to the tagret sum, the function should return an empty array
+# example array = [12,3,1,2,-6,5,-8,6], targetSum = 0
+# sample output should be [[-8,2,6], [-8,3,5], [-6,1,5]]
+
+def threeNumbersum(array, targetSum):
+    pass
